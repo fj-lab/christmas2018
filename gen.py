8d5fb287eea7e3c5baa43f002b3260bbffee2d85
baibai25 <kamuy.camel.0225@gmail.com>
2018-12-25 01:49:26 +0900

    First commit

---
+++ gen.py
@@ -0,0 +1,44 @@
+import os, sys
+import tifffile
+import numpy as np
+import keras
+from keras.models import load_model
+np.random.seed(42)
+
+# Save generated images as 32bit grayscale tiff
+def save_generated_images(generated_images):
+
+    for i in range(len(generated_images)):
+        image = generated_images[i, :, :, :]
+        image += 1
+        image *= 127.5
+
+        """
+        save method
+        """
+
+        #name = './generated/{}.tif'.format(str(i)) 
+        #tifffile.imsave(name, image)
+         
+
+# Training
+def test(model_path, generate_size):
+    # Load model
+    model = load_model(model_path)
+    
+    # Generate images
+    noise = np.random.normal(0, 1, size=(generate_size, ) + (1, 1, 100))
+    generated_images = model.predict(noise)
+    
+    # Output shape (generate_size, 64, 64, 1) 
+    save_generated_images(generated_images)
+
+  
+def main():
+    model_path = './models/generator_epoch*.hdf5'
+    generate_size = 64
+    test(model_path, generate_size)
+
+
+if __name__ == "__main__":
+    main()
