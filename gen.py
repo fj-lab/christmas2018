5f5d4dbeef7d25396281b90500bd45dfc0c47541
baibai25 <kamuy.camel.0225@gmail.com>
2018-12-26 02:04:26 +0900

    Update

--- gen.py
+++ gen.py
@@ -3,19 +3,34 @@ import tifffile
 import numpy as np
 import keras
 from keras.models import load_model
-from PIL import Image
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 np.random.seed(42)
 
 # Save generated images
 def save_generated_images(generated_images):
    
+    plt.figure(figsize=(8, 4))
+    gs = gridspec.GridSpec(4, 8)
+    gs.update(wspace=0, hspace=0)
+
     for i in range(len(generated_images)):
+        ax = plt.subplot(gs[i])
+        ax.set_aspect('equal')
+
         image = generated_images[i, :, :, :]
         image += 1
         image *= 127.5
-        name = './generated/{}.jpg'.format(str(i))
-        plt.imsave(name, image.astype(np.uint8))
+        #name = './generated/{}.jpg'.format(str(i))
+        #plt.imsave(name, image.astype(np.uint8))
+        fig = plt.imshow(image.astype(np.uint8))
+        plt.axis('off')
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+    
+    plt.tight_layout()
+    plt.savefig('gs.jpg', bbox_inches='tight', pad_inches=0)
+     
     #plt.imshow(image.astype(np.uint8))
     #plt.show()
 
@@ -32,8 +47,8 @@ def test(model_path, generate_size):
 
   
 def main():
-    model_path = './models/generator_epoch30.hdf5'
-    generate_size = 64
+    model_path = './models/generator_epoch200.hdf5'
+    generate_size = 32
     test(model_path, generate_size)
 
 
